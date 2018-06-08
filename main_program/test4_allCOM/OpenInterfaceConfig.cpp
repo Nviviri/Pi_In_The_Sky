b56bc94dcf5a75ebd53c7a2d16f0aefe363d13be
rivia <mattwhite0912@outlook.com>
2018-06-08 21:30:17 +0200

    add openinterface.cpp

---
+++ main_program/test4_allCOM/OpenInterfaceConfig.cpp
@@ -0,0 +1,28 @@
+#include "OpenInterfaceConfig.h"
+
+// Command data
+std::vector<uint8_t> startSafe()
+{
+   return {START, MODE_SAFE};
+}
+
+std::vector<uint8_t> modeSpot()
+{
+   return {MODE_SPOT};
+}
+
+std::vector<uint8_t> modeClean()
+{
+   return {MODE_CLEAN};
+}
+
+/// Velocities in mm/sec
+std::vector<uint8_t> driveDirect(int16_t rightVelocity, int16_t leftVelocity)
+{
+   return {DRIVE_DIRECT_4,
+           static_cast<uint8_t>(rightVelocity >> 8),
+           static_cast<uint8_t>(rightVelocity & 0x00ff),
+           static_cast<uint8_t>(leftVelocity >> 8),
+           static_cast<uint8_t>(leftVelocity & 0x00ff)
+   };
+}
