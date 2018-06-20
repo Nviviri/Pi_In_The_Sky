edd152a788e85137e9a46ad1f122b151ec937653
584394 <nviviri@gmail.com>
2018-06-20 12:06:48 +0200

    Updating Sensor data

--- main_program/test5_all/sensor.cpp
+++ main_program/test5_all/sensor.cpp
@@ -1,55 +1,82 @@
 #include "sensor.h"
+#include "SerialLink.h"
+#include "sensor.h"
+#include "OpenInterfaceConfig.h"
+#include "ParLoop.h"
+
 #include <iostream>
 
 using namespace std;
+extern SerialLink sl;
+
+Sensor::Sensor():
+  getSensorData_(std::bind(&Sensor::getSensor(), this), 1)
+{
+
+
+}
 
 void Sensor::initialise() 
 {
-	frontSensor_ = 0;
-	backSensor_ = 0;
-	leftSensor_ = 0;
-	rightSensor_ = 0;
-   cout << "Sensor init." << endl;
+  wall_ = 0;
+  cliffLeft_ = 0;
+  cliffRight_ = 0;
+  cliffFrontLeft_ = 0;
+  cliffFrontRight_ = 0;
+  charge_ = 0;
+  capacity_ = 0;
+  distance_ = 0;
+  angle_ = 0;
+  buttons_ = 0;
+  cout << "Sensor Initialised." << endl;
+  
+  
+}
+
+void Sensor::getSensor() 
+{
+ std::vector<uint8_t> data{};
+  data = sl.writeRead(reqAllData(),26);
+  wall_ = data[1];
+  buttons_ = data[11];
+  cliffLeft_ = data[2];
+  cliffRight_ = data[5];
+  cliffFrontLeft_ = data[3];
+  cliffFrontRight_ = data[4];
+  distance_ = (uint16_t)data[12] << 8) | data[12];
+  angle_ =(uint16_t)data[13] << 8) | data[13];
+  charge_ = ((uint16_t)data[22] << 8) | data[23];
+  capacity_ = ((uint16_t)data[24] << 8) | data[25];
+cout << "Read Sensors" << endl;
+
 }
 
 void Sensor::reset() 
 {
-	frontSensor_ = 0;
-	backSensor_ = 0;
-	leftSensor_ = 0;
-	rightSensor_ = 0;
+  wall_ = 0;
+  cliffLeft_ = 0;
+  cliffRight_ = 0;
+  cliffFrontLeft_ = 0;
+  cliffFrontRight_ = 0;
+  charge_ = 0;
+  capacity_ = 0;
+  distance_ = 0;
+  angle_ = 0;
+  buttons_ = 0;
    cout << "Sensor reset." << endl;
 }
 
 void Sensor::shutdown() 
 {
- 	frontSensor_ = 0;
-	backSensor_ = 0;
-	leftSensor_ = 0;
-	rightSensor_ = 0;
+  wall_ = 0;
+  cliffLeft_ = 0;
+  cliffRight_ = 0;
+  cliffFrontLeft_ = 0;
+  cliffFrontRight_ = 0;
+  charge_ = 0;
+  capacity_ = 0;
+  distance_ = 0;
+  angle_ = 0;
+  buttons_ = 0;
    cout << "Sensor shutdown." << endl;
-}
-
-void Sensor::setFrontSensor(int reading)
-{
-	frontSensor_ = reading;
-  cout << "reading sensor from roomba: " << reading << endl;
-}
-
-void Sensor::setBackSensor(int reading)
-{
-	backSensor_ = reading;
-  cout << "reading sensor from roomba: " << reading << endl;
-}
-
-void Sensor::setLeftSensor(int reading)
-{
-	leftSensor_ = reading;
-  cout << "reading sensor from roomba: " << reading << endl;
-}
-
-void Sensor::setRightSensor(int reading)
-{
-	rightSensor_ = reading;
-  cout << "reading sensor from roomba: " << reading << endl;
 }
--- main_program/test5_all/sensor.h
+++ main_program/test5_all/sensor.h
@@ -10,24 +10,25 @@ class Sensor: public Device
  Sensor(const std::string &id) : Device(id), frontSensor_{0}, backSensor_{0}, leftSensor_{0}, rightSensor_{0}{}
   virtual ~Sensor(){}
   virtual void initialise() override; // C++11 override specifier
+  virtual void getSensor() override;
   virtual void reset() override;
   virtual void shutdown() override;
-  void setFrontSensor(int reading);
-  void setBackSensor(int reading);
-  void setLeftSensor(int reading);
-  void setRightSensor(int reading);
-  int getFrontSensor() const {return frontSensor_;};
-  int getBackSensor() const{return backSensor_;};
-  int getLeftSensor() const{return leftSensor_;};
-  int getRighttSensor() const{return rightSensor_;};
+
 
  protected:
+  uint8_t wall_ = 0;
+  uint8_t cliffRight_ = 0;
+  uint8_t cliffFrontLeft_ = 0;
+  uint8_t cliffFrontRight_ = 0;
+  uint16_t charge_ = 0;
+  uint16_t capacity_ = 0;
+  uint16_t distance_ = 0;
+  uint16_t angle_ = 0;
+  uint8_t buttons_ = 0;
 
  private:
-  int frontSensor_;
-  int backSensor_;
-  int leftSensor_;
-  int rightSensor_;
+
+  
 };
 
 #endif // Motor_H
