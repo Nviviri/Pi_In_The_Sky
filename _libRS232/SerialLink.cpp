9cf82dfc2c349427c4c7493db1166fccf0ef6e4d
mattoakland <mattwhite0912@outlook.com>
2018-06-21 14:34:34 +0200

    fix mutex bug

--- _libRS232/SerialLink.cpp
+++ _libRS232/SerialLink.cpp
@@ -1,10 +1,14 @@
 #include "SerialLink.h"
+#include <thread>
+#include <mutex>
 
 /**
  *  \brief SerialLink::SerialLink starts a communication with an external device at a certain baud rate
  *  \param comPort is a std::string containing the 'name' of the port ('/dev/...')
  *  \param baud is of type unsigned int and contains the selected baudrate of the device
  **/
+std::mutex mtx;
+
 SerialLink::SerialLink(const std::string& comPort, 
                        const unsigned int baud, 
                        const unsigned int character_size, 
@@ -58,8 +62,10 @@ void SerialLink::write(const std::vector<uint8_t>& dat) {
     }
     try {
       //std::lock_guard<std::mutex> lock(com_lock);
+      mtx.lock();
         boost::asio::write(port_, boost::asio::buffer(dat));
         //port_.write_some(boost::asio::buffer(dat));
+	mtx.unlock();
     }
     catch(const std::exception& e) {
         std::cerr << "ERROR: " << e.what() << '\n';
@@ -76,9 +82,11 @@ std::vector<uint8_t> SerialLink::read(const size_t read_size) {
     std::vector<uint8_t> dat;
     try {
       //std::lock_guard<std::mutex> lock(com_lock);
+      mtx.lock();
         dat.resize(read_size);
         boost::asio::read(port_, boost::asio::buffer(dat, read_size));
         //port_.read_some(boost::asio::buffer(dat, read_size));
+	mtx.unlock();
     }
     catch(const std::exception& e) {
         std::cerr << "ERROR: " << e.what() << '\n';
@@ -96,7 +104,11 @@ std::vector<uint8_t> SerialLink::read(const size_t read_size) {
  **/
 std::vector<uint8_t> SerialLink::writeRead(const std::vector<uint8_t>& data_out, const size_t read_size) {
   //	std::lock_guard<std::mutex> lock(com_lock);
-    write(data_out);
-    return read(read_size);
+  std::vector<uint8_t> ret{};
+  //mtx.lock();
+  write(data_out);
+  ret = read(read_size);
+  //mtx.unlock();
+  return ret;
 }
 
