bd8969892ad071b37a013b63101bca0df31ca814
584394 <nviviri@gmail.com>
2018-06-21 13:07:38 +0200

    Fixed topic

--- _libMQTT/MQTTconfig.h
+++ _libMQTT/MQTTconfig.h
@@ -18,7 +18,7 @@ const bool MQTT_RETAIN_ON{true};
 const std::string MQTT_LOCAL_BROKER{"127.0.0.1"};
 const std::string MQTT_PUBLIC_BROKER{"ws://broker.hivemq.com:8000/mqtt"};
 const int MQTT_LOCAL_BROKER_PORT{1883};
-const std::string MQTT_TOPIC_ROOT{"ESEiot/" + GROUP + "HOSTNAME"};
+const std::string MQTT_TOPIC_ROOT{"ESEiot/" + GROUP + HOSTNAME};
 
 //const std::string MQTT_LOCAL_BROKER{"127.0.0.1"};
 //const int MQTT_LOCAL_BROKER_PORT{1883};
